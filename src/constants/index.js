631b7c3e41807aaca3cb35850f303aae825fbbb0
nashdabre <nashdabre2@gmail.com>
2025-06-17 18:27:38 +0530

    final changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -188,33 +188,6 @@ const experiences = [
   },
 ];
 
-const testimonials = [
-  {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-];
-
 const projects = [
   {
     name: "Placement Trainee",
@@ -235,7 +208,7 @@ const projects = [
       },
     ],
     image: placement_trainee,
-    video: "/videos/landslide.mp4", 
+    video: "/videos/Placement_trainee.mp4", 
     source_code_link: "https://github.com/nashdabre/Placement-Trainee",
   },
    {
@@ -287,4 +260,4 @@ const projects = [
  
 ];
 
-export { services, technologies, experiences, testimonials, projects };
+export { services, technologies, experiences,  projects };
